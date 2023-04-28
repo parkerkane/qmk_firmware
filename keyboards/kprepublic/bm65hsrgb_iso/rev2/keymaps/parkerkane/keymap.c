724be62436dbf38853f5af848b6ffa204b281076
Timo Reunanen <parker@friikz.net>
2023-04-29 00:47:41 +0300

    Added colors to inform if UNI mode enabled

--- keyboards/kprepublic/bm65hsrgb_iso/rev2/keymaps/parkerkane/keymap.c
+++ keyboards/kprepublic/bm65hsrgb_iso/rev2/keymaps/parkerkane/keymap.c
@@ -20,8 +20,6 @@
 enum layers {
 	_BASE,
     _EXT,
-	_ADV,
-	_FN,
 	_UNI,
 };
 
@@ -43,7 +41,7 @@ const uint32_t unicode_map[] PROGMEM = {
 #define KC_OUML XP(OUML_SMALL, OUML_BIG)
 
 #define MO_EXT MO(_EXT)
-#define MO_UNI MO(_UNI)
+#define TG_UNI TG(_UNI)
 #define MO_UNI2 MO(_UNI2)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -52,23 +50,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_PGUP,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_PGDN,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,  KC_HOME,
-        SC_LSPO, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SC_RSPC, KC_UP,   KC_END,
-        KC_LCTL, KC_LGUI, KC_LALT,                   LT(_UNI,KC_SPC),                    KC_RALT, MO_EXT,           KC_LEFT, KC_DOWN, KC_RGHT
+        SC_LSPO,KC_GRAVE, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_END,
+        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                    KC_RALT, MO_EXT,           KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     [_EXT] = LAYOUT_65_iso_blocker(
-        KC_GRAVE,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, _______, _______,
+        _______,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,   KC_DEL, _______,
         _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, _______, _______,          _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______,  UC_WIN, UC_WINC, UC_LINX,  UC_MAC, _______, QK_BOOT, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______,                            _______,                   _______, _______,          _______, _______, _______
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_OUML, KC_AUML, _______, _______, _______,
+        _______, _______, UC_WINC,  UC_MAC, _______, _______, QK_BOOT, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______,                            _______,                    TG_UNI, _______,          _______, _______, _______
     ),
 
 
     [_UNI] = LAYOUT_65_iso_blocker(
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_OUML, _______, _______, _______,          _______,
-        _______, KC_AUML, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+         TG_UNI, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_OUML, KC_AUML, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______,                            _______,                   _______, _______,          _______, _______, _______
     ),
@@ -88,3 +86,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 */
 
 };
+
+#ifdef RGB_MATRIX_ENABLE
+
+static bool uniState = false;
+
+// __attribute__ ((weak))
+bool rgb_matrix_indicators_user(void)
+{
+    if (uniState)
+    {
+        int i;
+        for (i = 16; i < 27; i++) rgb_matrix_set_color(i, 0xFF, 0x00, 0x00);
+        for (i = 31; i < 42; i++) rgb_matrix_set_color(i, 0xFF, 0x00, 0x00);
+        for (i = 46; i < 53; i++) rgb_matrix_set_color(i, 0xFF, 0x00, 0x00);
+    }
+    return true;
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    // rgblight_set_layer_state(2, layer_state_cmp(state, _FN));
+    // rgblight_set_layer_state(3, layer_state_cmp(state, _ADJUST));
+
+    uniState = layer_state_cmp(state, _UNI);
+
+    return state;
+}
+
+#endif
